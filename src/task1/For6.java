2e08c451e3fc025b1bf1faf492223a5af4e754c3
BekirovArs <bekirovarslan410@gmail.com>
2021-05-23 14:30:48 +0300

    tasks  1 to 15

---
+++ src/task1/For6.java
@@ -0,0 +1,10 @@
+package task1;
+
+import java.util.Scanner;
+
+public class For6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
---
+++ src/task1/java7.java
@@ -0,0 +1,10 @@
+package task1;
+
+public class java7 {
+    public static void main(String[] args) {
+        double a = 6;
+        double b = 3.14;
+        System.out.println(2*a*b);
+        System.out.println(b*(a*a));
+    }
+}
